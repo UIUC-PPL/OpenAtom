dc06f5877241bb1db1cec894d51f038e6c678472
Lawlietgit <q.l.lawliet@gmail.com>
2018-02-19 16:54:48 -0500

    removed old versions

--- PAW_develop/gen_Gauss_quad/driver.h
+++
@@ -1,7 +0,0 @@
-
-void check_nodes_uniform(int, double *);
-void check_nodes_Gausshalf(int, double *);
-void fetch_Int_xpow_uniform(int, double *); 
-void fetch_Int_xpow_Gaussfull(int, double *); 
-void fetch_Int_xpow_Gausshalf(int, double *);
-
